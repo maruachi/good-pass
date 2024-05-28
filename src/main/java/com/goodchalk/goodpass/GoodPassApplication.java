bd1c1dead80ecc2de824359b8f9f5ea6a1d07bae
dgyim <yimdgkr@gmail.com>
2024-05-29 00:33:43 +0900

    스프링 프로젝트 생성

---
+++ src/main/java/com/goodchalk/goodpass/GoodPassApplication.java
@@ -0,0 +1,13 @@
+package com.goodchalk.goodpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodPassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goodchalk/goodpass/GoodPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodchalk.goodpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodPassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
